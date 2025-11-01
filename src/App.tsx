2cc66066d60ca84ded52729ba96801fff2e8e9d3
Joseph Angelo Flerimé <jflerime@toxiostek.com>
2025-11-01 04:17:23 -0400

    remove all css and translate to tailwindcss and new animation

--- src/App.tsx
+++ src/App.tsx
@@ -14,66 +14,38 @@ function App() {
   const thirdSectionRef = useRef<HTMLElement>(null);
 
   useGSAP(() => {
-    // gsap.fromTo(
-    //   ".hibou-title",
-    //   { y: 0 },
-    //   { y: "120dvh", duration: 3.5, ease: "elastic" },
-    // );
-    //
-    gsap.to(".hibou-title", {
-      y: "150dvh",
-      ease: "power2.out",
-      duration: 3,
-      scrollSmoother: {
-        wrapper: "first-section",
-        content: "hibou-title",
-        smooth: true,
-        effects: true,
-        normalizeScroll: true,
-      },
-      scrollTrigger: {
-        trigger: ".hibou-title",
-        start: "top top",
-        end: "bottom+=150dvh top", // how long to scroll
-        scrub: true,
-        pin: true,
-      },
-    });
-
-    gsap.to(".box-1", {
-      x: 300,
-      y: 300,
-      ease: "power2.out",
-      duration: 6, 
-    });
-
-    gsap.fromTo(".box-2", {
-      x: 300,
-      y: 300,
-    }, {
-      x: 0,
-      y: 0,
-      ease: "power2.out",
-      duration: 6, 
-    });
-
-    gsap.from(".box-3", {
+    gsap.to(".test-section .box-1", {
       x:300,
-      y:300,
       ease: "power2.inOut",
       duration: 3,
-      
+    })
+    gsap.to(".test-section .box-2", {
+      x:400,
+      ease: "power2.inOut",
+      duration: 3,
+    })
+    gsap.to(".test-section .box-3", {
+      x:500,
+      ease: "power2.inOut",
+      duration: 3,
+    })
+
+    gsap.to(".test-section .box-4", {
+      x:600,
+      ease: "power2.inOut",
+      duration: 3,
     })
   });
 
   return (
     <div ref={mainPageRef} className="main-page">
       <section className="test-section section">
-        <div className="box-1"></div>
+        <div className="box-1 w-[100px] h-[100px] bg-red-500"></div>
         <div className="box-2 w-[100px] h-[100px] bg-blue-500">
           <h1 className="text-[10px] text-white p-4">Welcome to the right place</h1>
         </div>
-        <div className="box-3 absolute w-[100px] h-[100px] bg-green-500"></div>
+        <div className="box-3 w-[100px] h-[100px] bg-green-500"></div>
+        <div className="box-4 w-[100px] h-[100px] bg-black left-0 top-0"></div>
       </section>
       <section ref={firstSectionRef} className="first-section section">
         {/* <picture>
